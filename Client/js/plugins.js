50d53a3c3936bb1d55fc42bffb29589927495ed3
kyubuns <kyubuns@gmail.com>
2015-11-29 17:26:09 +0900

    ツイートするキャラ作った

--- Client/js/plugins.js
+++ Client/js/plugins.js
@@ -4,5 +4,6 @@ var $plugins =
 [
 {"name":"MVCommons","status":true,"description":"Great utility library that provides common-use and\nsimplified functions. Also expands default classes.","parameters":{}},
 {"name":"OrangeCustomEvents","status":true,"description":"This plugin Will let you add or copy events to the current map","parameters":{}},
-{"name":"Network","status":true,"description":"","parameters":{}}
+{"name":"Network","status":true,"description":"","parameters":{}},
+{"name":"Tweet","status":true,"description":"ツイートするやつ","parameters":{}}
 ];
